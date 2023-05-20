28b3ffee54d2db71ab72b7c44c7cbeacb25c4920
Danu Pratama <pratamadhanuaji@gmail.com>
2023-05-20 21:00:48 +0700

    Initial commit

---
+++ src/main/java/com/example/v2absensicapil/V2AbsensiCapilApplication.java
@@ -0,0 +1,13 @@
+package com.example.v2absensicapil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2AbsensiCapilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(V2AbsensiCapilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/v2absensicapil/V2AbsensiCapilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.v2absensicapil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2AbsensiCapilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
